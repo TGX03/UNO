5f8cbfd0bc16d86f5ea509c191d03a66578db8db
TGX03 <tgx003@gmail.com>
2022-02-01 10:24:47 +0100

    Improve TODO

--- src/main/java/de/tgx03/uno/host/Host.java
+++ src/main/java/de/tgx03/uno/host/Host.java
@@ -102,7 +102,7 @@ public class Host implements Runnable {
 	 * @param handler The handler to remove.
 	 */
 	public void removeExceptionHandler(@NotNull ExceptionHandler handler) {
-		synchronized (exceptionHandlers) {  // TODO: Check the issue that occurs when not doing this in a separate thread.
+		synchronized (exceptionHandlers) {  // TODO: When removing an exception handler while handling an exception a concurrent modification exception occurs.
 			exceptionHandlers.remove(handler);
 		}
 	}
